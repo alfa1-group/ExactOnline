6c99bf584dd58588703a2cbc6fdb2a00f7de8203
Stef Heyenrath <stef.heyenrath@gmail.com>
2025-08-08 08:51:34 +0200

    --models

--- examples/ConsoleApp/Program.cs
+++ examples/ConsoleApp/Program.cs
@@ -23,38 +23,7 @@ var client = scope.ServiceProvider.GetRequiredService<ExactOnlineServiceClient>(
 
 var meResponse = await client.Api.V1.Current.Me.GetAsync(a =>
 {
-    a.QueryParameters.Select = "ID,Email,DisplayName,FirstName,LastName,Language,Locale,TimeZone";
+    // a.QueryParameters.Select = "ID,Email,FirstName,LastName";
 });
 var me = meResponse?.ToItem();
-
-var accountancyAccountInvolvedAccountsResponse = await client.Api.V1["abc"].Accountancy.AccountInvolvedAccounts.GetAsync(r =>
-{
-    r.QueryParameters.Filter = "ID eq guid'3fa85f64-5717-4562-b3fc-2c963f66afa6'";
-});
-var accountancyAccountInvolvedAccounts = accountancyAccountInvolvedAccountsResponse?.ToResults();
-
-await client.Api.V1["abc"].Accountancy.AccountInvolvedAccountsWithId(new Guid("3fa85f64-5717-4562-b3fc-2c963f66afa6")).PutAsync(new AccountancyAccountInvolvedAccounts(), r =>
-{
-});
-
-int xxxx = 0;
-
-var settings = new ODataClientSettings
-{
-    BaseUri = new Uri("https://start.exactonline.nl/api/v1/"),
-    BeforeRequestAsync = (r) =>
-    {
-        return Task.CompletedTask;
-    }
-};
-
-var oclient = new ODataClient("http://packages.nuget.org/v1/FeedService.svc/");
-
-var packages = await oclient
-    .For<SystemSystemMe>()
-    .Filter(x => x.Title == "Simple.OData.Client")
-    .FindEntryAsync();
-//foreach (var package in packages)
-//{
-//    Console.WriteLine(package.Title);
-//}
+Console.WriteLine($"Hello {me?.CurrentDivision} {me?.Email}");
