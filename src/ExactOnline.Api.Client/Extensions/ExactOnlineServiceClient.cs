9e883da17bdd6fd1ef8ec192eaf69527df923cc7
Stef Heyenrath <stef.heyenrath@gmail.com>
2025-08-12 18:10:51 +0200

    ordering

--- src/ExactOnline.Api.Client/Extensions/ExactOnlineServiceClient.cs
+++ src/ExactOnline.Api.Client/Extensions/ExactOnlineServiceClient.cs
@@ -22,20 +22,39 @@ public partial class ExactOnlineServiceClient
 
     private static HttpClientRequestAdapter CreateHttpClientRequestAdapter(IAuthenticationProvider authenticationProvider, ExactOnlineRateLimitHandler exactOnlineRateLimitHandler)
     {
+        var handlers = BuildDelegatingHandlers(exactOnlineRateLimitHandler);
+        var jsonParseNodeFactory = BuildJsonParseNodeFactory();
+
+        return new HttpClientRequestAdapter(authenticationProvider, jsonParseNodeFactory, httpClient: KiotaClientFactory.Create(handlers))
+        {
+            BaseUrl = DefaultBaseUrl
+        };
+    }
+
+    private static IList<DelegatingHandler> BuildDelegatingHandlers(ExactOnlineRateLimitHandler exactOnlineRateLimitHandler)
+    {
         var handlers = KiotaClientFactory.CreateDefaultHandlers();
 
+        // 1. Insert the GuidWrappingHandler after the UriReplacementHandler.
         var urlReplacementHandlerPosition = handlers.Select((handler, index) => new { handler, index }).FirstOrDefault(dg => dg.handler is UriReplacementHandler<UriReplacementHandlerOption>)?.index ?? -1;
         if (urlReplacementHandlerPosition != -1)
         {
             handlers.Insert(urlReplacementHandlerPosition + 1, new GuidWrappingHandler());
         }
 
+        // 2. Replace the RetryHandler by ExactOnlineRateLimitHandler.
         var retryHandlerPosition = handlers.Select((handler, index) => new { handler, index }).FirstOrDefault(dg => dg.handler is RetryHandler)?.index ?? -1;
         if (retryHandlerPosition != -1)
         {
+            handlers.RemoveAt(retryHandlerPosition);
             handlers.Insert(retryHandlerPosition, exactOnlineRateLimitHandler);
         }
 
+        return handlers;
+    }
+
+    private static JsonParseNodeFactory BuildJsonParseNodeFactory()
+    {
         // 1. Create JsonSerializerOptions and add the custom Legacy DateTime converters.
         var customOptions = new JsonSerializerOptions
         {
@@ -51,10 +70,6 @@ public partial class ExactOnlineServiceClient
 
         // 3. Create the JsonParseNodeFactory with the KiotaJsonSerializationContext.
         var jsonParseNodeFactory = new JsonParseNodeFactory(kiotaJsonSerializationContext);
-
-        return new HttpClientRequestAdapter(authenticationProvider, jsonParseNodeFactory, httpClient: KiotaClientFactory.Create(handlers))
-        {
-            BaseUrl = DefaultBaseUrl
-        };
+        return jsonParseNodeFactory;
     }
 }
