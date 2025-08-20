3da5d6eff886e4e59dc80cecdeb970baf64ccf02
Stef Heyenrath <stef.heyenrath@gmail.com>
2025-08-20 14:15:55 +0200

    fix build

--- src/ExactOnline.Api.Client.Interfaces/IExactOnlineServiceClient.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://github.com/StefH/ProxyInterfaceSourceGenerator
-
-using ProxyInterfaceGenerator;
-
-namespace ExactOnline.Api.Client;
-
-[Proxy<ExactOnlineServiceClient>(true)]
-public partial interface IExactOnlineServiceClient;
