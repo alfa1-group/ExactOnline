f3120bed62f7e96a70808e4cbaf745bbf75922df
Stef Heyenrath <stef.heyenrath@gmail.com>
2025-08-22 08:17:50 +0200

    Add TryGet... methods to DeletedEntityType

--- test/ExactOnline.Api.Client.Tests/Constants/ConstantsTests.cs
+++ test/ExactOnline.Api.Client.Tests/Constants/ConstantsTests.cs
@@ -38,4 +38,38 @@ public class ConstantsTests
 
         await Assert.That(result).IsFalse();
     }
+
+    [Test]
+    public async Task DeletedEntityType_TryGetName_Known_Int_To_String()
+    {
+        var result = DeletedEntityType.TryGetName(14, out var entityTypeName);
+
+        await Assert.That(result).IsTrue();
+        await Assert.That(entityTypeName).IsEqualTo("SyncProjectTimeCostTransactions");
+    }
+
+    [Test]
+    public async Task DeletedEntityType_TryGetName_Unknown_Int_To_String()
+    {
+        var result = DeletedEntityType.TryGetName(500, out _);
+
+        await Assert.That(result).IsFalse();
+    }
+
+    [Test]
+    public async Task DeletedEntityType_TryGetType_Known_Int_To_Type()
+    {
+        var result = DeletedEntityType.TryGetType(3, out var entityType);
+
+        await Assert.That(result).IsTrue();
+        await Assert.That(entityType).IsEqualTo(typeof(SyncCRMAddress));
+    }
+
+    [Test]
+    public async Task DeletedEntityType_TryGetType_Unknown_Int_To_Type()
+    {
+        var result = DeletedEntityType.TryGetType(500, out _);
+
+        await Assert.That(result).IsFalse();
+    }
 }
